f261432016cb88ef8477fbff18710e0ecf3c389a
iKAN2025 <bellsparents@gmail.com>
2024-07-02 14:38:01 -0700

    rey

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -11,7 +11,7 @@ export var javaURI;
 if (location.hostname === "localhost") {
         javaURI = "http://localhost:8085";
 } else if (location.hostname === "127.0.0.1") {
-        javaURI = "http://127.0.0.1:8085";
+        javaURI = "http://127.0.0.1:8085"; //rey
 } else {
         javaURI = "https://spring.nighthawkcodingsociety.com";
 }
