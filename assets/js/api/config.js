d9bfc36d2456b92c750ba3d38b62cdc911f2ff99
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2024-07-08 12:04:04 -0700

    change port on config.js

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,10 +1,10 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-        pythonURI = "http://localhost:8086";
+        pythonURI = "http://localhost:8087";
 } else if (location.hostname === "127.0.0.1") {
-        pythonURI = "http://127.0.0.1:8086";
+        pythonURI = "http://127.0.0.1:8087";
 } else {
-        pythonURI = "http://127.0.0.1:8086";
+        pythonURI = "http://127.0.0.1:8087";
 }
 
 export var javaURI;
