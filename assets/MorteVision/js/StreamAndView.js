853c0782a9c9f8d8237f8ffaa5e3b33d1b878fb6
spooketti <jonliu168@gmail.com>
2024-11-06 21:00:09 -0800

    not gonna reimplemnt rtc cause i need to get a reasonable amount of sleep tonight

--- assets/MorteVision/js/StreamAndView.js
+++ assets/MorteVision/js/StreamAndView.js
@@ -1,11 +1,14 @@
 let peerConnection
 
-async function startStream() {
+async function startStream()
+{
     let stream
-    try {
+    try
+    {
         stream = await captureScreen()
     }
-    catch {
+    catch
+    {
         console.error("can u js be normal man")
     }
     if (!peerConnection) {
@@ -14,15 +17,18 @@ async function startStream() {
     stream.getTracks().forEach((track) => peerConnection.addTrack(track, stream));
 }
 
-async function viewStream() {
-    if (!peerConnection) {
+async function viewStream()
+{
+    if(!peerConnection)
+    {
         peerConnection = createPeerConnection()
     }
-    if (peerConnection.getTransceivers().length === 0) {
-        peerConnection.addTransceiver("video", { direction: "recvonly" })
-        peerConnection.addTransceiver("audio", { direction: "recvonly" })
-        document.getElementById("streamOffline").style.display = "none"
-        document.getElementById("mortStream").style.display = "block"
+    if(peerConnection.getTransceivers().length === 0)
+    {
+    peerConnection.addTransceiver("video", { direction: "recvonly" })
+    peerConnection.addTransceiver("audio", { direction: "recvonly" })
+    document.getElementById("streamOffline").style.display = "none"
+    document.getElementById("mortStream").style.display = "block"
     }
 
     await peerNegotiation()
@@ -47,38 +53,25 @@ async function captureScreen() {
 }
 
 
-function createServerPeerConnection() //streamer calls this one
-{
-    const peer = new RTCPeerConnection(servers)
-
-    peer.onnegotiationneeded = () => serverPeerNegotiation()
-    peer.onicecandidate = ({ candidate }) => {
-        if (candidate && signalingServer.readyState === signalingServer.OPEN) {
-            signalingServer.send(JSON.stringify({ target: id, type: 'candidate', payload: candidate }));
-        }
-    };
-    return peer
-}
-
-function createClientPeerConnection() //reciever calls this one
+function createPeerConnection()
 {
     const peer = new RTCPeerConnection(servers)
 
     peer.addTransceiver('video', {
         direction: 'recvonly'
-    });
-    peer.addTransceiver('audio', {
+      });
+      peer.addTransceiver('audio', {
         direction: 'recvonly'
-    });
+      });
 
     peer.onnegotiationneeded = () => peerNegotiation()
     peer.onicecandidate = ({ candidate }) => {
         if (candidate && signalingServer.readyState === signalingServer.OPEN) {
-            signalingServer.send(JSON.stringify({ target: id, type: 'candidate', payload: candidate }));
+          signalingServer.send(JSON.stringify({ target: id, type: 'candidate', payload: candidate }));
         }
-    };
-    peer.ontrack = ({ streams }) => {
-        console.log("recieved data")
+      };
+    peer.ontrack = ({streams}) => {
+        console.log("life sucks")
         document.getElementById("mortStream").srcObject = streams[0]
         document.getElementById("mortStream").style.display = "block"
         document.getElementById("streamOffline").style.display = "none"
@@ -87,62 +80,27 @@ function createClientPeerConnection() //reciever calls this one
 }
 
 
-
-async function serverPeerNegotiation() {
-    // if(!peerConnection)
-    // {
-    //     peerConnection = createPeerConnection()
-    // }
-    // const offer = await peerConnection.createOffer();
-    // await peerConnection.setLocalDescription(offer);
-    // signalingServer.send(JSON.stringify({ target: id, type: 'offer', payload: offer }));
-    if (peerConnection) {
-        peerConnection.close()
+async function peerNegotiation()
+{
+    if(!peerConnection)
+    {
+        peerConnection = createPeerConnection()
     }
-    peerConnection = createStreamerPeerConnection()
-    const offer = await peer.createOffer();
-    await peer.setLocalDescription(offer);
-    const payload = {
-        sdp: peer.localDescription
-    };
-
-    const { data } = await axios.post('/broadcast', payload);
-    const desc = new RTCSessionDescription(data.sdp);
-    peer.setRemoteDescription(desc).catch(e => console.log(e));
-}
-
-async function clientPeerNegotiation() {
-    // if(!peerConnection)
-    // {
-    //     peerConnection = createPeerConnection()
-    // }
-    // const offer = await peerConnection.createOffer();
-    // await peerConnection.setLocalDescription(offer);
-    // signalingServer.send(JSON.stringify({ target: id, type: 'offer', payload: offer }));
-    const peer = new RTCPeerConnection({ servers })
-
-    peer.ontrack = (e) => handleTrackEvent(e, peer);
-    const desc = new webrtc.RTCSessionDescription(body.sdp);
-    await peer.setRemoteDescription(desc);
-    const answer = await peer.createAnswer();
-    await peer.setLocalDescription(answer);
-    const payload = {
-        sdp: peer.localDescription
-    }
-
-    res.json(payload);
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    signalingServer.send(JSON.stringify({ target: id, type: 'offer', payload: offer }));
 
 }
-
 
 let id
-signalingServer.onmessage = async (message) => {
+signalingServer.onmessage = async(message) => {
     const data = JSON.parse(message.data)
     console.log(data.type)
-    switch (data.type) {
+    switch(data.type)
+    {
         case "init":
             id = data.id
-            break;
+        break;
 
         case "offer":
             if (!peerConnection) {
@@ -153,21 +111,21 @@ signalingServer.onmessage = async (message) => {
             await peerConnection.setLocalDescription(answer); // Set the local description
             signalingServer.send(JSON.stringify({ target: data.payload.id, type: 'answer', payload: answer }));
             // signalingServer.send(JSON.stringify({description: peerConnection.localDescription}));
-            break;
+        break;
 
         case "answer":
             if (!peerConnection) {
                 peerConnection = createPeerConnection();
             }
             await peerConnection.setRemoteDescription(new RTCSessionDescription(data.payload));
-            break;
+        break;
 
         case "candidate":
             if (!peerConnection) {
                 peerConnection = createPeerConnection();
             }
             await peerConnection.addIceCandidate(new RTCIceCandidate(data.payload));
-            break;
+        break;
     }
 }
 
