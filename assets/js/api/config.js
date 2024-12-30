308c337597849230ff0b0eed9ba8a6cf37011d4e
jm1021 <jmort1021@gmail.com>
2024-12-30 08:44:51 -0800

    Restore top login, add toolkit inside of login menu

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,6 +1,6 @@
 export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-    pythonURI = "http://localhost:8085";  // Same URI for localhost or 127.0.0.1
+    pythonURI = "http://localhost:8087";  // Same URI for localhost or 127.0.0.1
 } else {
     pythonURI = "https://flask2025.nighthawkcodingsociety.com";
 }
