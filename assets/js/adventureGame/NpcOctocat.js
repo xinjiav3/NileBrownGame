a15ad653e2bb8e6b97130c9fc39662a2b79ac79e
jm1021 <jmort1021@gmail.com>
2025-01-23 18:24:12 -0800

    add github messages

--- assets/js/adventureGame/NpcOctocat.js
+++ assets/js/adventureGame/NpcOctocat.js
@@ -1,19 +1,20 @@
 import Npc from "./Npc.js";
 
-// Array of Linux command questions
-const quiz = "Linux Command Quiz";
+// Array of GitHub command questions
+const quiz = "GitHub Command Quiz";
 const questions = [
-    "Which command is used to create a new directory?\n1. mkdir\n2. rmdir\n3. touch\n4. rm",
-    "Which command is used to change the current directory?\n1. cd\n2. ls\n3. pwd\n4. mv",
-    "Which command lists the contents of a directory?\n1. ls\n2. cd\n3. mkdir\n4. rm",
-    "Which command is used to remove a file?\n1. rm\n2. rmdir\n3. del\n4. erase",
-    "Which command is used to display the current directory path?\n1. pwd\n2. path\n3. cd\n4. dir",
-    "Which command is used to run a script?\n1. ./script.sh\n2. run script.sh\n3. exec script.sh\n4. start script.sh",
-    "Which command is used to copy files?\n1. cp\n2. mv\n3. copy\n4. duplicate",
-    "Which command is used to move or rename files?\n1. mv\n2. cp\n3. move\n4. rename",
-    "Which command is used to display the contents of a file?\n1. cat\n2. show\n3. display\n4. view",
-    "Which command is used to create an empty file?\n1. touch\n2. create\n3. new\n4. mkfile"
+    "Which command is used to clone a repository?\n1. git clone\n2. git fork\n3. git copy\n4. git download",
+    "Which command is used to add changes to the staging area?\n1. git add\n2. git stage\n3. git commit\n4. git push",
+    "Which command is used to commit changes?\n1. git commit\n2. git add\n3. git save\n4. git push",
+    "Which command is used to push changes to a remote repository?\n1. git push\n2. git upload\n3. git send\n4. git commit",
+    "Which command is used to pull changes from a remote repository?\n1. git pull\n2. git fetch\n3. git receive\n4. git update",
+    "Which command is used to check the status of the working directory and staging area?\n1. git status\n2. git check\n3. git info\n4. git log",
+    "Which command is used to create a new branch?\n1. git branch\n2. git create-branch\n3. git new-branch\n4. git checkout",
+    "Which command is used to switch to a different branch?\n1. git checkout\n2. git switch\n3. git change-branch\n4. git branch",
+    "Which command is used to merge branches?\n1. git merge\n2. git combine\n3. git join\n4. git integrate",
+    "Which command is used to view the commit history?\n1. git log\n2. git history\n3. git commits\n4. git show"
 ];
+
 class NpcOctocat extends Npc {
     constructor(data = null) {
         super(data, quiz, questions);
