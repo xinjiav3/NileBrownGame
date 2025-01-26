52f454bf47284feec64eca283205a8062f20f843
jm1021 <jmort1021@gmail.com>
2025-01-26 14:11:54 -0800

    switch to case style

--- assets/js/adventureGame/PlayerChillGuy.js
+++ assets/js/adventureGame/PlayerChillGuy.js
@@ -14,12 +14,14 @@ class PlayerChillGuy extends Player1 {
     }
 
     handleCollisionAction(objectID) {
-        if (objectID === "Octocat") {
-            alert("Hi I am Octocat! I am the friendly GitHub mascot.  Code, Code, Code!");
-        } else if (objectID === "Tux") {
-            alert("Hi I am Tux, the Linux mascot.  I am very happy to spend some shell time with you!"); 
+        switch (objectID) {
+            case "Octocat":
+                alert("Hi I am Octocat! I am the GitHub code code code collaboration mascot");
+                break;
+            case "Tux":
+                alert("Hi I am Tux, the Linux mascot.  I am very happy to spend some shell time with you!"); 
+                break;
         }
-        // Empty method to be overridden by subclasses if needed
     }
     
 }
